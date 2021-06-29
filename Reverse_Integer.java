8ed85bcbef449b7c74f538c9fc3de9e684ae10ba
abhijitmondal121 <mondalavi9836@gmail.com>
2021-06-30 00:18:10 +0530

    u

---
+++ Reverse_Integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int reverse(int x) {
+          int result=0;
+        int prev=0;
+        
+        while(x!=0){
+            int temp=x%10;
+            x=x/10;
+            result=(result*10)+temp;
+            
+            if((result-temp)/10!=prev){
+                return 0;
+            }
+            else{
+                prev=result;
+            }
+        }
+        return result;
+        
+        
+        
+        
+    }
+}
