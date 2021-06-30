a98c26f74ec42b34e5c96772cd8d27fe3130dbb2
abhijitmondal121 <mondalavi9836@gmail.com>
2021-07-01 00:10:21 +0530

    in

---
+++ Search_Insert_Position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low=0;
+        int heigh=nums.length-1;
+        while(low<=heigh){
+            int mid=low +(heigh-low)/2;
+            
+            if(nums[mid]==target){
+                return mid;
+                
+            }
+            else if(nums[mid]>target){
+                heigh=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
