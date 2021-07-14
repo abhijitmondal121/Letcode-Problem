69c0396e069d17367458c08e7bcbf3574075de8a
abhijitmondal121 <mondalavi9836@gmail.com>
2021-07-15 00:04:15 +0530

    init

---
+++ binarySearch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int l=0,r=nums.length-1,mid=0;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+           
+        }
+         return -1;
+    }
+}
